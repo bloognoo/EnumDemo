898a25ea610c0edde81da3d599240b4278ba213f
Harry French <harry.french@moonforge.com>
2021-10-25 20:25:24 +0100

    Working Demo

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    internal class Program
+    {
+        enum Stuff
+        {
+            ThingA=0, // this is important 
+            ThingB,
+            ThingC,
+            ThingD
+        }
+        
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            var stuffCount = Enum.GetNames(typeof(Stuff)).Length;
+            var thingInt = rand.Next(stuffCount);
+            var thingStr = (Stuff)thingInt;
+
+            Console.WriteLine("Thing Is : {0}",thingInt);
+            Console.WriteLine("Thing is also : {0}",thingStr);
+        }
+    }
+}
